a48eac2926bbd14618e6d38eaf6dae9fa534aa2c
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-06-21 02:00:15 -0600

    Add MaintenanceMode component

---
+++ src/domains/site-management/MaintenanceMode/MaintenanceMode.tsx
@@ -0,0 +1,11 @@
+export const MaintenanceMode = () => (
+  <main className="h-dvh grid justify-items-center items-center">
+    <div className="grid justify-items-center p-5 rounded-md bg-slate-900 backdrop-blur-sm">
+      <h1 className="text-xl">Jhonny Vargas Arias</h1>
+      <span className="text-sm">Portfolio</span>
+      <div className="border-t border-gray-300 w-2/4 my-4"></div>
+      <p>This page is currently under development.</p>
+      <p>Stay tuned for updates!</p>
+    </div>
+  </main>
+);
