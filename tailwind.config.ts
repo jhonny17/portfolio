9e70b36c705fc9d99f05a4ae67a51bed6c1c9e39
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-06-21 01:35:17 -0600

    Modify config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/domains/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
