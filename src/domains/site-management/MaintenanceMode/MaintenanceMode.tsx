865b0deba666f07ee32c33b3149933c74a54a1f5
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-07-05 23:19:48 -0600

    Remove css file, does not work as expected

--- src/domains/site-management/MaintenanceMode/MaintenanceMode.tsx
+++ src/domains/site-management/MaintenanceMode/MaintenanceMode.tsx
@@ -1,16 +1,12 @@
-import cx from 'classnames';
-
-import cl from './MaintenanceMode.module.css';
-
 export const MaintenanceMode = () => (
-  <main className={cl.maintenanceMode}>
-    <div className={cl.container}>
-      <div className={cx(cl.lightEffect, 'animate-pulse')} />
-      <div className={cl.card}>
-        <div className={cl.cardContainer}>
-          <h1 className={cl.title}>Jhonny Vargas Arias</h1>
-          <span className={cl.subtitle}>Portfolio</span>
-          <div className={cl.divider} />
+  <main className="h-dvh grid justify-items-center items-center">
+    <div className="relative p-6 min-w-max aspect-video">
+      <div className="absolute inset-0 filter blur-3xl rounded-full bg-gradient-to-r from-emerald-500 from-20% via-teal-800 via-50% to-emerald-500 to-90% animate-pulse" />
+      <div className="p-8 bg-slate-950 bg-opacity-30 rounded-lg backdrop-blur">
+        <div className="grid justify-items-center">
+          <h1 className="text-2xl font-serif">Jhonny Vargas Arias</h1>
+          <span className="text-sm tracking-wide">Portfolio</span>
+          <div className="border-t border-gray-300 w-2/4 my-4" />
           <p>This page is currently under development.</p>
           <p>Stay tuned for updates!</p>
         </div>
