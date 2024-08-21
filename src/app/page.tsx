c2967bd2540051a79e58dd84457abb5b5dd0ae46
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-08-20 23:32:03 -0600

    Complete rollback in the use of api routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-import { use } from 'react';
-
+import { FeatureFlag } from '@/utils';
 import { MaintenanceMode } from '@/domains/site-management';
 import { USE_MAINTENANCE_MODE } from '@/constants/feature-flags';
 
-const fetchUseMaintenanceMode = async () => {
-  const result = await fetch(
-    `${process.env.HOST}/api/v1/feature-flag/${USE_MAINTENANCE_MODE}`,
-    {
-      method: 'GET',
-    },
-  );
-  const { isEnabled } = await result.json();
-  return isEnabled;
-};
-
 export default async function Home() {
-  const useMaintenanceMode = await fetchUseMaintenanceMode();
+  const useMaintenanceMode = await FeatureFlag.isEnabled(USE_MAINTENANCE_MODE);
 
   if (useMaintenanceMode) {
     return <MaintenanceMode />;
