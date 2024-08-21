199d08fcaf9b01323aec337ae453aed2c58a9b25
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-08-21 00:12:25 -0600

    Rollback on server feature flag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { FeatureFlag } from '@/utils';
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { MaintenanceMode } from '@/domains/site-management';
 import { USE_MAINTENANCE_MODE } from '@/constants/feature-flags';
 
-export default async function Home() {
-  const useMaintenanceMode = await FeatureFlag.isEnabled(USE_MAINTENANCE_MODE);
+export default function Home() {
+  const [useMaintenanceMode, setUseMaintenanceMode] = useState(false);
+
+  useEffect(() => {
+    const fetchMaintenanceMode = async () => {};
+
+    fetchMaintenanceMode();
+  });
 
   if (useMaintenanceMode) {
     return <MaintenanceMode />;
