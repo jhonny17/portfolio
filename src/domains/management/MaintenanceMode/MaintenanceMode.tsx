41c1f701917f75b05d4ffad7ebc074e29ed79446
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-08-25 02:06:55 -0600

    Hotfix: Change color to white

--- src/domains/management/MaintenanceMode/MaintenanceMode.tsx
+++ src/domains/management/MaintenanceMode/MaintenanceMode.tsx
@@ -22,11 +22,15 @@ export const MaintenanceMode = ({ children }: MaintenanceModeProps) => {
         <div className="absolute inset-0 filter blur-3xl rounded-full bg-gradient-to-r from-emerald-500 from-20% via-teal-800 via-50% to-emerald-500 to-90% animate-pulse" />
         <div className="p-8 bg-slate-950 bg-opacity-30 rounded-lg backdrop-blur">
           <div className="grid justify-items-center">
-            <h1 className="text-2xl font-serif">Jhonny Vargas Arias</h1>
-            <span className="text-sm tracking-wide">Portfolio</span>
+            <h1 className="text-2xl font-serif text-white">
+              Jhonny Vargas Arias
+            </h1>
+            <span className="text-sm tracking-wide text-white">Portfolio</span>
             <div className="border-t border-gray-300 w-2/4 my-4" />
-            <p>This page is currently under development.</p>
-            <p>Stay tuned for updates!</p>
+            <p className="text-white">
+              This page is currently under development.
+            </p>
+            <p className="text-white">Stay tuned for updates!</p>
           </div>
         </div>
       </div>
