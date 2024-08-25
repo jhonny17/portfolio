af7de374b7fd9940fd16b7af6a060e6f16241ca4
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-08-25 01:43:49 -0600

    Modify theme with new vision

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 import type { Config } from 'tailwindcss';
 
+const MAX_WIDTH = 1440;
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,6 +16,15 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        top: '0 -1px 3px 0 var(--tw-shadow-color)',
+      },
+      maxWidth: {
+        '8xl': `${MAX_WIDTH}px`,
+      },
+      screens: {
+        '2xl': `${MAX_WIDTH}px`,
       },
     },
   },
