82cdd04c0efc5907d2898926760a17c7706cce54
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-10-06 04:16:59 -0600

    Make commit contribution responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,16 @@ const username = process.env.NEXT_PUBLIC_GITHUB_USERNAME;
 
 export default function Home() {
   return (
-    <div className={cx('grid', 'justify-center')}>
+    <div
+      className={cx(
+        'flex',
+        'justify-center',
+        'relative',
+        'overflow-x-auto',
+        'px-4',
+        'py-2',
+      )}
+    >
       <GitHubCalendar username={username} />
     </div>
   );
