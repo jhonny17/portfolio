f64220b0d09a1c2e658ee2de65ee9be39a9a4862
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-11-04 21:18:06 -0600

    Add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { Contact } from '@/sections/Contact';
+
+const ContactPage = () => {
+  return (
+    <>
+      <h1 className="sr-only">Contact</h1>
+      <Contact />
+    </>
+  );
+};
+
+export default ContactPage;
